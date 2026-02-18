903d8536bc357c682a1a8fc5358164a3b716d6c7
Mehemmed Tacoğlu <muhammettacoglu31@gmail.com>
2026-02-18 19:15:49 +0300

    Add files via upload

---
+++ dairecevrealan.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    float r,alan,cevre;
+    const float pi = 3.1415;
+
+    printf("Dairenin yaricapini giriniz");
+    scanf("%f", &r);
+
+    alan = pi * r * r;
+    cevre = 2 * pi * r;
+
+    printf("Alan: %f\n", alan);
+    printf("cevre: %f\n", cevre);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
